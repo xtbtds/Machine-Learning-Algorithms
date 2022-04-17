356a858efc55479b69cb231b682d63dcffc55a3f
xtbtds <liza.xtbtds@gmail.com>
2022-04-17 13:11:42 +0300

    tested logreg

--- LogisticRegression/logistic_regression.py
+++ LogisticRegression/logistic_regression.py
@@ -1,39 +1,42 @@
+import numpy as np
+import pandas as pd
+import scipy.linalg as sla
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.metrics import mean_squared_error
+
+"""
+LOGISTIC REGRESSION - 
+Predict probability of classes
+"""
+
 def logit(x, w):
     return np.dot(x, w)
 
 def sigmoid(h):
     return 1. / (1 + np.exp(-h))
 
+
 class MyLogisticRegression(object):
     def __init__(self):
         self.w = None
     
     def fit(self, X, y, max_iter=100, lr=0.1):
-        # Принимает на вход X, y и вычисляет веса по данной выборке.
-        # Множество допустимых классов: {1, -1}
-        # Не забудьте про фиктивный признак равный 1!
-        
         n, k = X.shape
-        
         if self.w is None:
             self.w = np.random.randn(k + 1)
-        
         X_train = np.concatenate((np.ones((n, 1)), X), axis=1)
-        
         losses = []
-        
         for iter_num in range(max_iter):
             z = sigmoid(logit(X_train, self.w))
             grad = np.dot(X_train.T, (z - y)) / len(y)
-
             self.w -= grad * lr
-
             losses.append(self.__loss(y, z))
-        
         return losses
         
     def predict_proba(self, X):
-        # Принимает на вход X и возвращает ответы модели
         n, k = X.shape
         X_ = np.concatenate((np.ones((n, 1)), X), axis=1)
         return sigmoid(logit(X_, self.w))
@@ -48,56 +51,36 @@ class MyLogisticRegression(object):
         p = np.clip(p, 1e-10, 1 - 1e-10)
         return np.mean(y * np.log(p) + (1 - y) * np.log(1 - p))
 
+
+"""
+TESTING
+"""
+
 from sklearn.datasets import make_blobs
 X, y = make_blobs(n_samples=1000, centers=[[-2,0.5],[2,-0.5]], cluster_std=1, random_state=42)
-
 colors = ("red", "green")
 colored_y = np.zeros(y.size, dtype=str)
-
 for i, cl in enumerate([0,1]):
-    colored_y[y == cl] = str(colors[i])
-    
+    colored_y[y == cl] = str(colors[i])  
 plt.figure(figsize=(15,10))
 plt.scatter(X[:, 0], X[:, 1], c=colored_y)
-plt.show()
+plt.show()      #DATA
+
 
 clf = MyLogisticRegression()
-
 clf.fit(X, y, max_iter=1000)
-
 w = clf.get_weights()
 
+
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-
 plt.figure(figsize=(15,8))
-
 eps = 0.1
 xx, yy = np.meshgrid(np.linspace(np.min(X[:,0]) - eps, np.max(X[:,0]) + eps, 500),
                      np.linspace(np.min(X[:,1]) - eps, np.max(X[:,1]) + eps, 500))
 
 Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-
 Z = Z.reshape(xx.shape)
-
 cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA'])
 plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
-
 plt.scatter(X[:, 0], X[:, 1], c=colored_y)
 
-colors = ("magenta", "green")
-colored_y = np.zeros(y.size, dtype=str)
-
-for i, cl in enumerate([0,1]):
-    colored_y[y == cl] = str(colors[i])
-
-
-plt.figure(figsize=(15,8))
-
-Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
-
-Z = Z.reshape(xx.shape)
-plt.pcolormesh(xx, yy, Z, cmap=plt.get_cmap('viridis'))
-
-plt.scatter(X[:, 0], X[:, 1], c=colored_y)
-plt.colorbar()
-plt.show()
